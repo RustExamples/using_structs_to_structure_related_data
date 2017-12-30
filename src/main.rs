4b26474c80909f2480753702e24aeb748a6f6a4f
Meenakshi Sundaram <vms20591@gmail.com>
2017-12-31 01:23:45 +0530

    Creating Instances From Other Instances With Struct Update Syntax

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,10 @@ fn main() {
         active: true
     };
 
-    println!("Email for `{}` is `{}`", u_vms.username, u_vms.email);
-
-    // use "mut" to modify fields of a struct
-    let mut u_vms = User {
-        username: String::from("vms20591"),
-        email: String::from("vms20591@gmail.com"),
-        sign_in_count: 0,
-        active: true
+    // Create instance from another with "Struct update syntax"
+    let u_xyz = User {
+        username: String::from("xyz"),
+        email: String::from("xyz@gmail.com"),
+        ..u_vms // Fill in other fields from u_vms instance
     };
-
-    u_vms.email = String::from("vms20591@protonmail.com");
-
-    println!("Email for `{}` is `{}`", u_vms.username, u_vms.email);
-
-    let new_user = build_user(u_vms.username.clone(), u_vms.email.clone());
-}
-
-fn build_user(username: String, email: String) -> User {
-    User {
-        username,
-        email,
-        active: true,
-        sign_in_count: 0
-    }
 }
