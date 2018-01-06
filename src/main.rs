017c257efbe89e7c09c7c21336712794050725cb
Meenakshi Sundaram <vms20591@gmail.com>
2018-01-06 21:20:22 +0530

    Rectangles with Tuples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let width1 = 30;
     let height1 = 50;
+
+    let rect1 = (width1, height1);
     
-    println!("Area of rectangle is {} sq. pixels", area(width1, height1));
+    println!("Area of rectangle is {} sq. pixels", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
